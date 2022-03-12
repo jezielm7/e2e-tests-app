298461a21f089dc9e94c858f33201cb1730c4da0
jeziel <jezielmarques7@gmail.com>
2022-03-11 21:12:04 -0300

    finished e2e tests

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,23 +1,44 @@
-describe('Example', () => {
+describe('Render successfully', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
 
-  beforeEach(async () => {
-    await device.reloadReactNative();
+  // beforeEach(async () => {
+  //   await device.reloadReactNative();
+  // });
+
+  it('should have info about cancel subscription', async () => {
+    const info = await element(by.text('Cancel anytime. Ofter terms apply.'));
+    await expect(info).toBeVisible();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it('should render value of basic plan', async () => {
+    const value = await element(by.text('5.25'));
+    await expect(value).toBeVisible();
   });
+});
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
+describe('Select Premium Plan', () => {
+  it('should subscribe to the premium plan', async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1500));
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+    await element(by.id('option-premium')).tap();
+
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    await element(by.id('input-email')).tap();
+    await element(by.id('input-email')).typeText('test@email.com');
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    await element(by.id('keyboard')).tap();
+
+    await element(by.id('button-subscribe')).tap();
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    await device.takeScreenshot('premium-plan-snapshot');
+
+    await expect(element(by.id('confirmation-message'))).toBeVisible();
   });
 });
